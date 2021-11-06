276338d828e80da744bf2cdd57b47ed002b41bb7
Juan Carlos Garcia <juancarlosg7494@gmail.com>
2021-11-05 20:54:17 -0500

    Agregar archivos de proyecto.

---
+++ ExamenT3/Controllers/NotaController.cs
@@ -0,0 +1,56 @@
+﻿using ExamenT3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamenT3.Controllers
+{
+    public class NotaController : Controller
+    {
+
+        public List<Notas> notas;
+        public NotaController( )
+        {
+   
+        }
+        public IActionResult Index()
+        {
+            return View(notas);
+        }
+        [HttpGet]
+        public IActionResult _Index(string search)
+        { 
+
+         
+            return View( );
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+         
+            return View("Create", new Notas());
+        }
+        [HttpPost]
+        public IActionResult Create(Notas nota)
+        {
+            addNotas(nota);
+            return View();
+             
+        }
+
+        
+
+        public List<Notas> addNotas(Notas nota) {
+
+            
+            var notas = new List<Notas> ();
+
+            notas.Add(nota);
+
+            return notas;
+            
+        }
+    }
+}
---
+++ ExamenT3/Models/Notas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamenT3.Models
+{
+    public class Notas
+    {
+        public int id { set; get; }
+      
+        public string Titulo { set; get; } 
+        public string Contenido { set; get; }
+     
+        
+    }
+}
